6c292df4dde379e8e4701fb37fc92d767b1785eb
BrodyBrosius <mcochran477@gmail.com>
2022-06-01 01:27:47 -0400

    Modified gitignore folder correctly. Removed node_modules folder

--- server/routes/login.cjs
+++ server/routes/login.cjs
@@ -3,15 +3,17 @@ const router = express.Router();
 const { createQLDBDriver } = require('../qldbdriversetup.cjs');
 const { v4: uuidv4 } = require('uuid');
 
-router.get('/login', async function(req, res) {
-    console.log("LOL");
+router.get('/message', async function(req, res) {
+    res.send("LOL");
 })
 
 
-router.post('/login', async function(req, res) {
+router.post('/logindata', async function(req, res) {
     const driver = createQLDBDriver();
     console.log(req.body.user_id_field);
     console.log(req.body.password_field);
+
+    res.send('done');
 
 })
 
