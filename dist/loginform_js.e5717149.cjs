58b288cae2c7f0c7f2bce06b67d2959daecb2ba5
BrodyBrosius <mcochran477@gmail.com>
2022-06-11 00:34:54 -0400

    another n_m update

---
+++ dist/loginform_js.e5717149.cjs
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $("#login_form").on("submit", function(event){
+        event.preventDefault();
+        let raw_login_pkg = document.getElementById("login_form");
+        let formData = new FormData(raw_login_pkg);
+        
+        login_data = {};
+
+        formData.forEach((value, key) => { //Populates login_data object with HTML form data and also validates multi-select inputs
+            if(!Reflect.has(login_data, key)){
+                login_data[key] = value;
+                return;
+            }
+            if(!Array.isArray(login_data[key])){
+                login_data[key] = [login_data[key]];
+            }
+            login_data[key].push(value);
+        });
+
+        $.ajax({
+            url:"http://localhost:3000/login/logindata", 
+            method: "POST",
+            data: login_data,
+            success: function(res) {
+                console.log(res);
+            },
+            error: function(res) {
+                console.log('error!');
+            }
+        });
+
+    })
+})
---
+++ dist/newpatientpage.b22c9fd4.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $("#patient_info").on("submit", function(event) {
+        event.preventDefault();
+        let raw_form_input = document.getElementById("patient_info");
+        let formData = new FormData(raw_form_input);
+        let patient_data = {};
+        formData.forEach((value, key)=>{
+            if (!Reflect.has(patient_data, key)) {
+                patient_data[key] = value;
+                return;
+            }
+            if (!Array.isArray(patient_data[key])) patient_data[key] = [
+                patient_data[key]
+            ];
+            patient_data[key].push(value);
+        });
+        try {
+            $.ajax({
+                url: "http://localhost:3000/patient",
+                method: "POST",
+                data: patient_data,
+                success: function(res) {
+                    try {
+                        document.querySelector('.newpatient-modal').style.display = 'flex';
+                        document.querySelector('.close').addEventListener('click', function() {
+                            document.querySelector('.newpatient-modal').style.display = 'none';
+                        });
+                        document.getElementById('ok_button').addEventListener('click', function() {
+                            document.querySelector('.newpatient-modal').style.display = 'none';
+                        });
+                    } catch (err) {
+                        alert("Patient Addition to database failed. Please try again or contact an administrator. Error: " + err);
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    try {
+                        alert("Patient Addition to database failed. Please try again or contact an administrator. Error: " + jqXHR.status);
+                    } catch (err) {
+                        alert("Patient Addition to database failed. Please try again or contact an administrator. Error: " + err);
+                    }
+                }
+            });
+        } catch (err) {
+            alert("Patient Addition to database failed. Please try again or contact an administrator. Error: " + err);
+        }
+    });
+});
+
+//# sourceMappingURL=newpatientpage.b22c9fd4.js.map
